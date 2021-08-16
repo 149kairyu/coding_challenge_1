55eeff875012b899bb100270da47b20a3ad2e766
149kairyu <75082935+149kairyu@users.noreply.github.com>
2021-08-16 13:22:01 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MainPage());
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  var _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Coding Challenge #1"),
+      ),
+      body: ElevatedButton(
+        onPressed: _incrementCounter,
+        child: Text("$_counter mal gedrückt!"),
+      ),
+    );
+  }
+}
